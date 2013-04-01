780f723d2855c8a7bb608ada0e615017f3bded34
Michael Arntzenius <daekharel@gmail.com>
2013-04-01 15:57:04 -0400

    fixes

--- script.sh
+++ script.sh
@@ -9,3 +9,6 @@ chmod +x run
 ./build
 echo "---------- DONE BUILDING ----------"
 echo "to run, almamater/run"
+echo "optional arguments: almamater/run LIMIT DOMAINNAME"
+echo "LIMIT: solutions worse than this LIMIT will not be submitted. default: 440"
+echo "DOMAINNAME: domain name to submit as. default: drama.cmu.edu"
